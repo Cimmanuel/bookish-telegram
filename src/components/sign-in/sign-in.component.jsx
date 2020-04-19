ccd1f4df9b8a2f549ec415b7a38d2161d3cf7bc4
Immanuel Kolapo <immanuelcaspian@gmail.com>
2020-04-19 03:16:47 +0100

    Integrated firebase for backend purposes, made a fucntional Google signin, created and styled SignIn component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
+import { signInWithGoogle } from '../../firebase/firebase.utils'
 import './sign-in.style.scss'
 
 class SignIn extends Component {
@@ -33,7 +34,10 @@ class SignIn extends Component {
 					<FormInput name="email" type="email" value={this.state.email} label="email" handleChange={this.handleChange} required />
 					<FormInput name="password" type="password" value={this.state.password} label="password" handleChange={this.handleChange} required />
 
-					<CustomButton type="submit">Sign in</CustomButton>
+					<div className="buttons">
+						<CustomButton type="submit">Sign in</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+					</div>
 				</form>
 			</div>
 		)
