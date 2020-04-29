d968c806957b4d5d457d3eb6802bb08aa701b4ae
Immanuel Kolapo <immanuelcaspian@gmail.com>
2020-04-29 17:28:46 +0100

    Removed certain keys

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-
-const config = {
-    apiKey: "AIzaSyC9RpzMV63OKGos7W6udhPhuDNQYSXNV2E",
-    authDomain: "ecommerce-968e4.firebaseapp.com",
-    databaseURL: "https://ecommerce-968e4.firebaseio.com",
-    projectId: "ecommerce-968e4",
-    storageBucket: "ecommerce-968e4.appspot.com",
-    messagingSenderId: "775471681884",
-    appId: "1:775471681884:web:a679d5562d0478a60e7199",
-    measurementId: "G-CW8F9HWS6G"
-}
+import config from './firebase.config'
 
 export const createUserProfileDocument = async (userAuthObject, additionalData) => {
 	if(!userAuthObject) return
